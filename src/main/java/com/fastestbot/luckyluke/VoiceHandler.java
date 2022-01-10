c8d9d91c8ac4836aac7e71a0ee14a7d029e5f64e
Luke Weetendorf <luke.weetendorf@fh-kiel.de>
2022-01-10 21:07:18 +0100

    added exception logging

--- src/main/java/com/fastestbot/luckyluke/VoiceHandler.java
+++ src/main/java/com/fastestbot/luckyluke/VoiceHandler.java
@@ -10,14 +10,16 @@ import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDateTime;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.List;
 
 
 /**
  * Quite badly written code, but I also couldn't be bothered to neither improve it nor to comment it, to bad.
- *
+ * <p>
  * Update ca. 2-5 Months later: Code isn't that bad anymore, but I can still not be bothered to comment it. It's selfexplanatory anyway (:
  */
 
@@ -28,21 +30,43 @@ public class VoiceHandler extends ListenerAdapter {
     HashMap<String, String> roleToVoiceID = new HashMap<>();
     HashMap<String, String> textToVoiceID = new HashMap<>();
 
-    private TextChannel createTextChannel(String name, Role role, Category parent){
+    private TextChannel createTextChannel(String name, Role role, Category parent) {
         EnumSet<Permission> allow = null;
         EnumSet<Permission> deny = EnumSet.of(Permission.MESSAGE_WRITE, Permission.MESSAGE_READ);
 
-        Main.appendToLogFile(LocalDateTime.now() + ": Creating Text Channel '" + name + "'.");
-        return guild.createTextChannel(name, parent).addRolePermissionOverride(everyoneID, allow, deny).addRolePermissionOverride(role.getIdLong(), deny, allow).complete();
+        List<TextChannel> textChannels = guild.getTextChannelsByName(name, true);
+        if (textChannels.isEmpty()) {
+            Main.appendToLogFile(LocalDateTime.now() + ": Creating Text Channel '" + name + "'.");
+            try {
+                return guild.createTextChannel(name, parent).addRolePermissionOverride(everyoneID, allow, deny).addRolePermissionOverride(role.getIdLong(), deny, allow).complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
+        } else {
+            Main.appendToLogFile(LocalDateTime.now() + ": Should create Channel '" + name + "', but it already existed.");
+            return textChannels.get(0);
+        }
+        return null;
     }
 
-    private Role createRole(String name){
-        Main.appendToLogFile(LocalDateTime.now() + ": Creating Role '" + name + "'.");
-        return guild.createRole().setName(name).complete();
+    private Role createRole(String name) {
+        List<Role> roles = guild.getRolesByName(name, true);
+        if (roles.isEmpty()) {
+            Main.appendToLogFile(LocalDateTime.now() + ": Creating Role '" + name + "'.");
+            try {
+                return guild.createRole().setName(name).complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
+        } else {
+            Main.appendToLogFile(LocalDateTime.now() + ": Should create Role '" + name + "', but it already existed.");
+            return roles.get(0);
+        }
+        return null;
     }
 
-    private void setGuild(Guild guild){
-        if (this.guild == null)this.guild = guild;
+    private void setGuild(Guild guild) {
+        if (this.guild == null) this.guild = guild;
     }
 
     @Override
@@ -57,16 +81,20 @@ public class VoiceHandler extends ListenerAdapter {
 
         Main.appendToLogFile(LocalDateTime.now() + ": '" + event.getMember().getEffectiveName() + "' joined the Channel '" + event.getChannelJoined().getName() + "'.");
 
-        if (!roleToVoiceID.containsKey(event.getChannelJoined().getId())){
+        if (!roleToVoiceID.containsKey(event.getChannelJoined().getId())) {
             role = createRole(roleName);
             textChannel = createTextChannel(roleName, role, parent);
 
             roleToVoiceID.put(event.getChannelJoined().getId(), role.getId());
             textToVoiceID.put(event.getChannelJoined().getId(), textChannel.getId());
-        }else{
+        } else {
             role = guild.getRoleById(roleToVoiceID.get(event.getChannelJoined().getId()));
         }
-        guild.addRoleToMember(event.getMember(), role).complete();
+        try {
+            guild.addRoleToMember(event.getMember(), role).complete();
+        } catch (Exception e) {
+            Main.appendToLogFile("Exception: " + e);
+        }
     }
 
     @Override
@@ -77,13 +105,27 @@ public class VoiceHandler extends ListenerAdapter {
 
         Main.appendToLogFile(LocalDateTime.now() + ": '" + event.getMember().getEffectiveName() + "' left the Channel '" + event.getChannelLeft().getName() + "'.");
 
-        guild.removeRoleFromMember(event.getMember(), channelRole).complete();
-        if (event.getChannelLeft().getMembers().isEmpty()){
-            Main.appendToLogFile(LocalDateTime.now() + ": Deleting Text Channel '" + guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId())).getName() + "'.");
-            guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+        try {
+            guild.removeRoleFromMember(event.getMember(), channelRole).complete();
+        } catch (Exception e) {
+            Main.appendToLogFile("Exception: " + e);
+        }
+        if (event.getChannelLeft().getMembers().isEmpty()) {
+            TextChannel channelToDelete = guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId()));
+            Main.appendToLogFile(LocalDateTime.now() + ": Deleting Text Channel '" + channelToDelete.getName() + "'.");
+            try {
+                channelToDelete.delete().complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
             textToVoiceID.remove(event.getChannelLeft().getId());
-            Main.appendToLogFile(LocalDateTime.now() + ": Deleting Role '" + guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId())).getName() + "'.");
-            guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+            Role roleToDelete = guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId()));
+            Main.appendToLogFile(LocalDateTime.now() + ": Deleting Role '" + roleToDelete.getName() + "'.");
+            try {
+                roleToDelete.delete().complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
             roleToVoiceID.remove(event.getChannelLeft().getId());
         }
     }
@@ -100,25 +142,41 @@ public class VoiceHandler extends ListenerAdapter {
 
         Main.appendToLogFile(LocalDateTime.now() + ": '" + event.getMember().getEffectiveName() + "' moved from the Channel '" + event.getChannelLeft().getName() + "' to the Channel '" + event.getChannelJoined().getName() + "'.");
 
-        guild.removeRoleFromMember(event.getMember(), role).complete();
+        try {
+            guild.removeRoleFromMember(event.getMember(), role).complete();
+        } catch (Exception e) {
+            Main.appendToLogFile("Exception: " + e);
+        }
         if (event.getChannelLeft().getMembers().isEmpty()) {
             Main.appendToLogFile(LocalDateTime.now() + ": Deleting Text Channel '" + guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId())).getName() + "'.");
-            guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+            try {
+                guild.getTextChannelById(textToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
             textToVoiceID.remove(event.getChannelLeft().getId());
             Main.appendToLogFile(LocalDateTime.now() + ": Deleting Role '" + guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId())).getName() + "'.");
-            guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+            try {
+                guild.getRoleById(roleToVoiceID.get(event.getChannelLeft().getId())).delete().complete();
+            } catch (Exception e) {
+                Main.appendToLogFile("Exception: " + e);
+            }
             roleToVoiceID.remove(event.getChannelLeft().getId());
         }
 
-        if (!roleToVoiceID.containsKey(event.getChannelJoined().getId())){
+        if (!roleToVoiceID.containsKey(event.getChannelJoined().getId())) {
             role = createRole(roleName);
             textChannel = createTextChannel(roleName, role, parent);
 
             roleToVoiceID.put(event.getChannelJoined().getId(), role.getId());
             textToVoiceID.put(event.getChannelJoined().getId(), textChannel.getId());
-        }else{
+        } else {
             role = guild.getRoleById(roleToVoiceID.get(event.getChannelJoined().getId()));
         }
-        guild.addRoleToMember(event.getMember(), role).complete();
+        try {
+            guild.addRoleToMember(event.getMember(), role).complete();
+        } catch (Exception e) {
+            Main.appendToLogFile("Exception: " + e);
+        }
     }
 }
